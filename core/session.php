c3a7bec3dd3989026373990c426ba97f6262735d
David Wibisono <davidwibisono95@gmail.com>
2017-11-23 14:49:19 +0700

    add session #4

--- core/session.php
+++ core/session.php
@@ -3,7 +3,7 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 include 'config.php';
 session_start();
-if (isset($_SESSION['user'])) {
+if (!isset($_SESSION['user'])) {
 	header("Location:".$baseurl.'/login');
 }
 var_dump(isset($_SESSION['user']));

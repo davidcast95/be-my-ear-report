cd852172610e3e32fa95fca7e7389c25a8ef70d5
David Wibisono <davidwibisono95@gmail.com>
2017-11-23 14:47:44 +0700

    add session #2

--- core/session.php
+++ core/session.php
@@ -1,8 +1,10 @@
 <?php 
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 include 'config.php';
 session_start();
 if (isset($_SESSION['user'])) {
 	header("Location:".$baseurl.'/login');
 }
-var_dump("tes");
+var_dump($baseurl.'/login');
  ?>

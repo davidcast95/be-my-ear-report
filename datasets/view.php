f398804d814cea7a13d5df459b26123fa69ad4e7
David Wibisono <davidwibisono95@gmail.com>
2017-11-23 13:35:23 +0700

    add play button to datasets #4

--- datasets/view.php
+++ datasets/view.php
@@ -49,7 +49,7 @@ include '../core/nav.php';
 		  	<td><?= $wav[$i] ?></td>
 		  	<td><?= $targets[$i] ?></td>
 		  	<td>
-		  		<audio id="<?= $wav[$i] ?>" src="<?= $root.'/'.$dir.'/'.$wav[$i].'.wav' ?>" preload="auto"></audio>
+		  		<audio id="<?= $wav[$i] ?>" src="<?= $dir.'/'.$wav[$i].'.wav' ?>" preload="auto"></audio>
 			  <button onclick="document.getElementById('<?= $wav[$i] ?>').play();">Play </button>
   			</td> 
 	  	</tr>

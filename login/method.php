8781c17611d502e792d796a5796d3948b67c37d6
David Wibisono <davidwibisono95@gmail.com>
2017-11-23 14:51:10 +0700

    add session #6

--- login/method.php
+++ login/method.php
@@ -5,6 +5,8 @@ if (isset($_GET['name'])) {
 	if ($method_name == 'login') {
 		if (validate_param($_POST, array('username','password'))) {
 			if ($_POST['username'] == 'admin' && $_POST['password'] == 'admin') {
+				session_start();
+				$_SESSION['user']= 'admin';
 				echo 200;
 			} else {
 				echo 401;

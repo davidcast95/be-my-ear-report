a6f3fe0e078812f1b3651dfc84781b8d2d5cc86c
David Wibisono <davidwibisono95@gmail.com>
2017-11-23 14:07:55 +0700

    add download section #8

--- reports/download.php
+++ reports/download.php
@@ -4,13 +4,14 @@ if (isset($_GET['name']) && isset($_GET['type'])) {
 	$model_name = $_GET['name'];
 	$filename = 'result_'.$_GET['type'].'.csv';
 
+	$filetarget = $models_dir.'/'.$model_name.'/reports/'.$filename;
 	// output headers so that the file is downloaded rather than displayed
 	// header('Content-Type: text/csv; charset=utf-8');
 	// header('Content-Disposition: attachment; filename=data.csv');
 	// create a file pointer connected to the output stream
-	$output = fopen($models_dir.'/'.$model_name.'/reports/'.$filename, 'r');
+	$output = fopen($filetarget, 'r');
 	var_dump($output);
 	// loop over the rows, outputting them
-	while ($row = fread($output, filesize($output))) echo $row;
+	while ($row = fread($filetarget)) echo $row;
 }
  ?>
